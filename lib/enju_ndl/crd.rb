d0a74d105cb5c9c463d87ad2778a989899740cc2
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2011-10-04 16:33:28 +0900

    added magic comment

--- lib/enju_ndl/crd.rb
+++ lib/enju_ndl/crd.rb
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 module EnjuNdl
   module Crd
     module ClassMethods
--- lib/enju_ndl/ndl_search.rb
+++ lib/enju_ndl/ndl_search.rb
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 module EnjuNdl
   module NdlSearch
     module ClassMethods
