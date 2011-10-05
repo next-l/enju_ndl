24b8c3ec204be968e1780a3781f60e8a45e65bf1
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2011-10-05 14:12:40 +0900

    added locale files

--- app/helpers/ndl_books_helper.rb
+++ app/helpers/ndl_books_helper.rb
@@ -3,7 +3,7 @@ module NdlBooksHelper
   def link_to_import(nbn)
     manifestation = Manifestation.where(:nbn => nbn).first
     unless manifestation
-      link_to '追加', ndl_books_path(:book => {:nbn => nbn}), :method => :post %>
+      link_to '追加', ndl_books_path(:book => {:nbn => nbn}), :method => :post
     end
   end
 end
