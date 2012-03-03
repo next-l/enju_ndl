062c1e96dd5e22f334a13b30ae1daad7d409a056
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2012-03-03 23:35:07 +0900

    do not import record that does not have JPNO

--- app/helpers/ndl_books_helper.rb
+++ app/helpers/ndl_books_helper.rb
@@ -1,11 +1,15 @@
 # -*- encoding: utf-8 -*-
 module NdlBooksHelper
   def link_to_import(nbn)
-    manifestation = Manifestation.where(:nbn => nbn).first
-    unless manifestation
-      link_to '追加', ndl_books_path(:book => {:nbn => nbn}), :method => :post
+    if nbn.blank?
+      t('enju_ndl.not_available')
     else
-      '登録済み'
+      manifestation = Manifestation.where(:nbn => nbn).first
+      unless manifestation
+        link_to '追加', ndl_books_path(:book => {:nbn => nbn}), :method => :post
+      else
+        '登録済み'
+      end
     end
   end
 end
