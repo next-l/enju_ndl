cf85391d616ff3346462007604ae731316940153
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2012-05-30 02:36:26 +0900

    updated dependency

--- lib/enju_ndl/engine.rb
+++ lib/enju_ndl/engine.rb
@@ -5,7 +5,7 @@ require 'isbn/tools'
 require 'acts_as_list'
 require 'attribute_normalizer'
 require 'library_stdnums'
-require 'enju_subject'
+require 'sunspot_rails'
 
 module EnjuNdl
   class Engine < ::Rails::Engine
--- lib/enju_ndl/version.rb
+++ lib/enju_ndl/version.rb
@@ -1,3 +1,3 @@
 module EnjuNdl
-  VERSION = "0.0.25"
+  VERSION = "0.0.26"
 end
