f4138574123aa4f6300fc16d7c55cbb1f01a24bd
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2012-07-24 16:00:19 +0900

    fixed pagination

--- app/controllers/ndl_books_controller.rb
+++ app/controllers/ndl_books_controller.rb
@@ -11,8 +11,8 @@ class NdlBooksController < ApplicationController
     @query = params[:query].to_s.strip
     books = NdlBook.search(params[:query], page)
     @books = Kaminari.paginate_array(
-      books[:items], :total_count => books[:total_entries]
-    )
+      books[:items], :total_count => books[:total_entries], :page => page
+    ).page(page)
 
     respond_to do |format|
       format.html
