532d743f1f88582473d5237f2c84527eb35ac5f3
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-02-23 18:21:42 +0900

    updated spec files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -38,6 +38,5 @@ RSpec.configure do |config|
 end
 
 VCR.configure do |c|
-  c.hook_into :fakeweb
   c.allow_http_connections_when_no_cassette = true
 end
--- spec/support/vcr.rb
+++
@@ -1,4 +0,0 @@
-VCR.configure do |c|
-  c.cassette_library_dir     = 'spec/cassette_library'
-  c.hook_into                :fakeweb
-end
