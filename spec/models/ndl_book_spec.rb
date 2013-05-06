417bb90af91df0789e936254be02b974bee9f270
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-05-07 04:54:29 +0900

    updated casette files

--- spec/models/ndl_book_spec.rb
+++ spec/models/ndl_book_spec.rb
@@ -10,7 +10,7 @@ describe NdlBook do
 
   context "search" do
     it "should search bibliographic record", :vcr => true do
-      NdlBook.search('library system')[:total_entries].should eq 5255
+      NdlBook.search('library system')[:total_entries].should eq 5258
     end
 
     it "should not distinguish double byte space from one-byte space in a query", :vcr => true do
