797050563cccdd9bbfa64ff6853d58f44170a2d4
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-08-23 20:30:46 +0900

    updated dummy file

--- spec/dummy/config/initializers/secret_token.rb
+++ spec/dummy/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = 'c92a5ffdf5d510686c2b2ecd8f4f10e7588b04640b1c9142f8aa9cb8f1f7f1b337429acf9452d1e2945dcb1166c83565c0e0fc3a3c291d502b89ab0758b09e47'
+Dummy::Application.config.secret_key_base = 'c92a5ffdf5d510686c2b2ecd8f4f10e7588b04640b1c9142f8aa9cb8f1f7f1b337429acf9452d1e2945dcb1166c83565c0e0fc3a3c291d502b89ab0758b09e47'
