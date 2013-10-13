fe3b5638e6147f9f68849d57074aa63cc6752b4e
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-10-14 07:15:01 +0900

    fixed helper

--- app/helpers/ndl_books_helper.rb
+++ app/helpers/ndl_books_helper.rb
@@ -1,15 +1,15 @@
 # -*- encoding: utf-8 -*-
 module NdlBooksHelper
-  def link_to_import_from_ndl(jpno)
-    if jpno.blank?
+  def link_to_import_from_ndl(iss_itemno)
+    if iss_itemno.blank?
       t('enju_ndl.not_available')
     else
-      identifier_type = IdentifierType.where(:name => 'jpno').first
+      identifier_type = IdentifierType.where(:name => 'iss_itemno').first
       if identifier_type
-        manifestation = Identifier.where(:body => jpno, :identifier_type_id => identifier_type.id).first.try(:manifestation)
+        manifestation = Identifier.where(:body => iss_itemno, :identifier_type_id => identifier_type.id).first.try(:manifestation)
       end
       unless manifestation
-        link_to t('enju_ndl.add'), ndl_books_path(:book => {:jpno => jpno}), :method => :post
+        link_to t('enju_ndl.add'), ndl_books_path(:book => {:iss_itemno => iss_itemno}), :method => :post
       else
         link_to t('enju_ndl.already_exists'), manifestation
       end
