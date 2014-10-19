deb02c20e484970560a7fc3fdcbe31898777ce2a
Kosuke Tanabe <nabeta@fastmail.fm>
2014-10-20 08:22:06 +0900

    updated dummy migration files

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141003182825) do
+ActiveRecord::Schema.define(version: 20141014065831) do
 
   create_table "agent_import_file_transitions", force: true do |t|
     t.string   "to_state"
@@ -245,6 +245,7 @@ ActiveRecord::Schema.define(version: 20141003182825) do
     t.integer  "lock_version",           default: 0, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "shelf_id"
   end
 
   add_index "checkouts", ["basket_id"], name: "index_checkouts_on_basket_id"
@@ -252,6 +253,7 @@ ActiveRecord::Schema.define(version: 20141003182825) do
   add_index "checkouts", ["item_id", "basket_id"], name: "index_checkouts_on_item_id_and_basket_id", unique: true
   add_index "checkouts", ["item_id"], name: "index_checkouts_on_item_id"
   add_index "checkouts", ["librarian_id"], name: "index_checkouts_on_librarian_id"
+  add_index "checkouts", ["shelf_id"], name: "index_checkouts_on_shelf_id"
   add_index "checkouts", ["user_id"], name: "index_checkouts_on_user_id"
 
   create_table "circulation_statuses", force: true do |t|
@@ -947,7 +949,6 @@ ActiveRecord::Schema.define(version: 20141003182825) do
     t.datetime "canceled_at"
     t.datetime "expired_at"
     t.datetime "deleted_at"
-    t.string   "state"
     t.boolean  "expiration_notice_to_patron",  default: false
     t.boolean  "expiration_notice_to_library", default: false
     t.datetime "retained_at"
@@ -958,7 +959,6 @@ ActiveRecord::Schema.define(version: 20141003182825) do
   add_index "reserves", ["item_id"], name: "index_reserves_on_item_id"
   add_index "reserves", ["manifestation_id"], name: "index_reserves_on_manifestation_id"
   add_index "reserves", ["request_status_type_id"], name: "index_reserves_on_request_status_type_id"
-  add_index "reserves", ["state"], name: "index_reserves_on_state"
   add_index "reserves", ["user_id"], name: "index_reserves_on_user_id"
 
   create_table "resource_export_file_transitions", force: true do |t|
