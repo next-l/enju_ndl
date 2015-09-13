b0610f8dc7a83e805e1fd41623014c4ba1d832eb
Kosuke Tanabe <nabeta@fastmail.fm>
2015-09-14 00:43:54 +0900

    update dummy migration files

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141014065831) do
+ActiveRecord::Schema.define(version: 20150425073705) do
 
   create_table "agent_import_file_transitions", force: :cascade do |t|
     t.string   "to_state"
@@ -32,9 +32,9 @@ ActiveRecord::Schema.define(version: 20141014065831) do
     t.integer  "user_id"
     t.text     "note"
     t.datetime "executed_at"
-    t.string   "agent_import_file_name"
+    t.string   "agent_import_filename"
     t.string   "agent_import_content_type"
-    t.integer  "agent_import_file_size"
+    t.integer  "agent_import_size"
     t.datetime "agent_import_updated_at"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -42,8 +42,10 @@ ActiveRecord::Schema.define(version: 20141014065831) do
     t.text     "error_message"
     t.string   "edit_mode"
     t.string   "user_encoding"
+    t.string   "agent_import_id"
   end
 
+  add_index "agent_import_files", ["agent_import_id"], name: "index_agent_import_files_on_agent_import_id"
   add_index "agent_import_files", ["parent_id"], name: "index_agent_import_files_on_parent_id"
   add_index "agent_import_files", ["user_id"], name: "index_agent_import_files_on_user_id"
 
@@ -168,12 +170,16 @@ ActiveRecord::Schema.define(version: 20141014065831) do
   add_index "carrier_type_has_checkout_types", ["checkout_type_id"], name: "index_carrier_type_has_checkout_types_on_checkout_type_id"
 
   create_table "carrier_types", force: :cascade do |t|
-    t.string   "name",         null: false
+    t.string   "name",                    null: false
     t.text     "display_name"
     t.text     "note"
     t.integer  "position"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "attachment_id"
+    t.string   "attachment_filename"
+    t.integer  "attachment_size"
+    t.string   "attachment_content_type"
   end
 
   create_table "checked_items", force: :cascade do |t|
@@ -292,12 +298,16 @@ ActiveRecord::Schema.define(version: 20141014065831) do
   add_index "classifications", ["parent_id"], name: "index_classifications_on_parent_id"
 
   create_table "content_types", force: :cascade do |t|
-    t.string   "name",         null: false
+    t.string   "name",                    null: false
     t.text     "display_name"
     t.text     "note"
     t.integer  "position"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "attachment_id"
+    t.string   "attachment_filename"
+    t.integer  "attachment_size"
+    t.string   "attachment_content_type"
   end
 
   create_table "countries", force: :cascade do |t|
@@ -706,9 +716,9 @@ ActiveRecord::Schema.define(version: 20141014065831) do
     t.integer  "required_score",                  default: 0,     null: false
     t.integer  "frequency_id",                    default: 1,     null: false
     t.boolean  "subscription_master",             default: false, null: false
-    t.string   "attachment_file_name"
+    t.string   "attachment_filename"
     t.string   "attachment_content_type"
-    t.integer  "attachment_file_size"
+    t.integer  "attachment_size"
     t.datetime "attachment_updated_at"
     t.text     "title_alternative_transcription"
     t.text     "description"
@@ -734,9 +744,12 @@ ActiveRecord::Schema.define(version: 20141014065831) do
     t.text     "publication_place"
     t.text     "extent"
     t.text     "dimensions"
+    t.string   "attachment_id"
+    t.string   "attachment_fingerprint"
   end
 
   add_index "manifestations", ["access_address"], name: "index_manifestations_on_access_address"
+  add_index "manifestations", ["attachment_id"], name: "index_manifestations_on_attachment_id"
   add_index "manifestations", ["date_of_publication"], name: "index_manifestations_on_date_of_publication"
   add_index "manifestations", ["doi"], name: "index_manifestations_on_doi"
   add_index "manifestations", ["manifestation_identifier"], name: "index_manifestations_on_manifestation_identifier"
@@ -849,15 +862,17 @@ ActiveRecord::Schema.define(version: 20141014065831) do
     t.integer  "position"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "picture_file_name"
+    t.string   "picture_filename"
     t.string   "picture_content_type"
     t.integer  "picture_file_size"
     t.datetime "picture_updated_at"
     t.text     "picture_meta"
     t.string   "picture_fingerprint"
+    t.string   "picture_id"
   end
 
   add_index "picture_files", ["picture_attachable_id", "picture_attachable_type"], name: "index_picture_files_on_picture_attachable_id_and_type"
+  add_index "picture_files", ["picture_id"], name: "index_picture_files_on_picture_id"
 
   create_table "produce_types", force: :cascade do |t|
     t.string   "name"
@@ -998,7 +1013,12 @@ ActiveRecord::Schema.define(version: 20141014065831) do
     t.datetime "executed_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "resource_export_id"
+    t.integer  "resource_export_size"
+    t.string   "resource_export_filename"
   end
+
+  add_index "resource_export_files", ["resource_export_id"], name: "index_resource_export_files_on_resource_export_id"
 
   create_table "resource_import_file_transitions", force: :cascade do |t|
     t.string   "to_state"
@@ -1019,9 +1039,9 @@ ActiveRecord::Schema.define(version: 20141014065831) do
     t.integer  "user_id"
     t.text     "note"
     t.datetime "executed_at"
-    t.string   "resource_import_file_name"
+    t.string   "resource_import_filename"
     t.string   "resource_import_content_type"
-    t.integer  "resource_import_file_size"
+    t.integer  "resource_import_size"
     t.datetime "resource_import_updated_at"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -1030,9 +1050,11 @@ ActiveRecord::Schema.define(version: 20141014065831) do
     t.text     "error_message"
     t.string   "user_encoding"
     t.integer  "default_shelf_id"
+    t.string   "resource_import_id"
   end
 
   add_index "resource_import_files", ["parent_id"], name: "index_resource_import_files_on_parent_id"
+  add_index "resource_import_files", ["resource_import_id"], name: "index_resource_import_files_on_resource_import_id"
   add_index "resource_import_files", ["user_id"], name: "index_resource_import_files_on_user_id"
 
   create_table "resource_import_results", force: :cascade do |t|
