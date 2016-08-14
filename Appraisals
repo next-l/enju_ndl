0d6536c4c8855ab9807973de477187fcff06d3ea
Kosuke Tanabe <nabeta@fastmail.fm>
2016-08-15 03:28:59 +0900

    update gemfiles

--- Appraisals
+++ Appraisals
@@ -1,13 +1,26 @@
 appraise "rails-4.2" do
   gem "rails", "~> 4.2.7"
+  gem 'statesman', '~> 1.3'
+end
+
+appraise "rails-5.0" do
+  gem "rails", "~> 5.0.0"
+  gem 'globalize', github: 'globalize/globalize'
+  gem 'statesman', '~> 2.0'
+  gem 'activemodel-serializers-xml'
+  gem 'rails-controller-testing'
+  gem 'sinatra', github: 'sinatra/sinatra'
+  gem 'rack-protection', github: 'sinatra/rack-protection'
 end
 
 appraise "rails-4.2-nii" do
   gem "rails", "~> 4.2.7"
-  gem 'enju_nii', '~> 0.2.0.beta.1'
+  gem 'statesman', '~> 1.3'
+  gem "enju_nii", "~> 0.2.0.beta.2"
 end
 
 appraise "rails-4.2-loc" do
   gem "rails", "~> 4.2.7"
-  gem 'enju_loc', '~> 0.2.0.beta.1'
+  gem 'statesman', '~> 1.3'
+  gem "enju_loc", "~> 0.2.0.beta.1"
 end
--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160813203039) do
+ActiveRecord::Schema.define(version: 20160814165332) do
 
   create_table "accepts", force: :cascade do |t|
     t.integer  "basket_id"
@@ -932,6 +932,7 @@ ActiveRecord::Schema.define(version: 20160813203039) do
     t.integer  "message_request_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "most_recent"
   end
 
   add_index "message_request_transitions", ["message_request_id"], name: "index_message_request_transitions_on_message_request_id"
@@ -962,11 +963,12 @@ ActiveRecord::Schema.define(version: 20160813203039) do
 
   create_table "message_transitions", force: :cascade do |t|
     t.string   "to_state"
-    t.text     "metadata",   default: "{}"
+    t.text     "metadata",    default: "{}"
     t.integer  "sort_key"
     t.integer  "message_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "most_recent"
   end
 
   add_index "message_transitions", ["message_id"], name: "index_message_transitions_on_message_id"
