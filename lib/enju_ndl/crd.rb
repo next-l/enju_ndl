c4d83b4c9163449570508b3063aadd3002dab994
Kosuke Tanabe <nabeta@fastmail.fm>
2017-01-11 20:25:24 +0900

    fix pagination

--- lib/enju_ndl/crd.rb
+++ lib/enju_ndl/crd.rb
@@ -7,7 +7,7 @@ module EnjuNdl
 
     module ClassMethods
       def get_crd_response(options)
-        params = {:query_logic => 1, :results_get_position => 1, :results_num => 200, :sort => 10}.merge(options)
+        params = {query_logic: 1, results_get_position: 1, results_num: 200, sort: 10}.merge(options)
         query = []
         query << "01_#{params[:query_01].to_s.gsub('　', ' ')}" if params[:query_01]
         query << "02_#{params[:query_02].to_s.gsub('　', ' ')}" if params[:query_02]
@@ -18,7 +18,7 @@ module EnjuNdl
       end
 
       def search_crd(options)
-        params = {:page => 1}.merge(options)
+        params = {page: 1}.merge(options)
         crd_page = params[:page].to_i
         crd_page = 1 if crd_page <= 0
         crd_startrecord = (crd_page - 1) * Question.crd_per_page + 1
@@ -29,20 +29,20 @@ module EnjuNdl
         doc = Nokogiri::XML(xml)
 
         response = {
-          :results_num => doc.at('//xmlns:hit_num').try(:content).to_i,
-          :results => []
+          results_num: doc.at('//xmlns:hit_num').try(:content).to_i,
+          results: []
         }
         doc.xpath('//xmlns:result').each do |result|
           set = {
-            :question => result.at('QUESTION').try(:content),
-            :reg_id => result.at('REG-ID').try(:content),
-            :answer => result.at('ANSWER').try(:content),
-            :crt_date => result.at('CRT-DATE').try(:content),
-            :solution => result.at('SOLUTION').try(:content),
-            :lib_id => result.at('LIB-ID').try(:content),
-            :lib_name => result.at('LIB-NAME').try(:content),
-            :url => result.at('URL').try(:content),
-            :ndc => result.at('NDC').try(:content)
+            question: result.at('QUESTION').try(:content),
+            reg_id: result.at('REG-ID').try(:content),
+            answer: result.at('ANSWER').try(:content),
+            crt_date: result.at('CRT-DATE').try(:content),
+            solution: result.at('SOLUTION').try(:content),
+            lib_id: result.at('LIB-ID').try(:content),
+            lib_name: result.at('LIB-NAME').try(:content),
+            url: result.at('URL').try(:content),
+            ndc: result.at('NDC').try(:content)
           }
           begin
             set[:keyword] = result.xpath('xmlns:KEYWORD').collect(&:content)
@@ -60,7 +60,7 @@ module EnjuNdl
         end
 
         resources = response[:results]
-        crd_results = Kaminari::paginate_array(resources, :total_count => crd_total_count, :page => crd_page).page(crd_page)
+        crd_results = Kaminari::paginate_array(resources, total_count: crd_total_count).page(crd_page)
       end
     end
   end
