f9160cb8e86dabc542e4dc719283f3ceb223547c
Kosuke Tanabe <nabeta@fastmail.fm>
2019-03-24 14:23:20 +0900

    update spec file

--- spec/models/import_request_spec.rb
+++ spec/models/import_request_spec.rb
@@ -6,4 +6,8 @@ describe ImportRequest do
   it "should import isbn", vcr: true do
     ImportRequest.import_isbn('4797327030').should be_valid
   end
+
+  it "should import a foreign book", vcr: true do
+    ImportRequest.import_isbn('0262220733').should be_valid
+  end
 end
